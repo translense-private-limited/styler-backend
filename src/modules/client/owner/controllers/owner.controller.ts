33570fa2919e97bb5c7571172510e6a133ccb3b8
atul singh <atul.singh@linktoany.com>
2024-10-04 15:22:11 +0530

    owner creation made public

--- src/modules/client/owner/controllers/owner.controller.ts
+++ src/modules/client/owner/controllers/owner.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { OwnerService } from "../services/owner.service";
 import { CreateOwnerDto } from "../dtos/owner.dto";
+import { Public } from "@src/utils/decorators/public.decorator";
 
 @Controller('owner')
 export class OwnerController {
@@ -12,7 +13,7 @@ export class OwnerController {
   }
 
   @Post()
-  @Post()
+  @Public()
   async createSeller(@Body() createOwnerDto: CreateOwnerDto) {
     console.log('create seller called ')
       return this.ownerService.createSeller(createOwnerDto);
