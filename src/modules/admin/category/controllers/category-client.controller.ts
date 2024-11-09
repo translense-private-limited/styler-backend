52db56276da37faa23d101e2d1e13e4f1df48c43
Prasanthi-Traslense <prasanthi.kimidi@translense.com>
2024-11-09 17:27:22 +0530

    category-client.controller.ts file has been created

---
+++ src/modules/admin/category/controllers/category-client.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from "@nestjs/common";
+import { CategoryService } from "../services/category.service";
+import { CategorySchema } from "../entities/category.schema";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+
+@Controller('client')
+@ApiTags('category/client')
+@ApiBearerAuth('jwt')
+
+export class CategoryClientController{
+    constructor(private readonly categoryService: CategoryService){}
+
+    @Get('categories')
+    async findAll(): Promise<CategorySchema[]> {
+        return this.categoryService.findAll();
+      }
+ 
+}
