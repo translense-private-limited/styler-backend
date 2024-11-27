c800439502450282b83978c7dde0fab74da691b8
Prasanthi-Traslense <prasanthi.kimidi@translense.com>
2024-11-27 15:14:29 +0530

    developed the endpoint customer/outlet/:outletId

--- src/modules/client/outlet/controllers/outlet-customer.controller.ts
+++ src/modules/client/outlet/controllers/outlet-customer.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { OutletCustomerService } from '../services/outlet-customer.service';
 import { NearbyOutletDto } from '../dtos/nearby-outlet.dto';
 import { Public } from '@src/utils/decorators/public.decorator';
+import { OutletService } from '../services/outlet.service';
 
 @Controller('customer')
+@ApiTags('customer/outlets')
 export class OutletCustomerController {
-  constructor(private outletCustomerService: OutletCustomerService) {}
+  constructor(private outletCustomerService: OutletCustomerService,
+    private outletService:OutletService
+  ) {}
 
   @Get('outlets')
   @Public()
@@ -16,5 +20,17 @@ export class OutletCustomerController {
   })
   async getNearbyOutlet(@Query() nearbyOutletDto: NearbyOutletDto) {
     return this.outletCustomerService.getNearbyOutlet(nearbyOutletDto);
+  }
+
+  @Get('outlet/:outletId')
+  @Public()
+  @ApiOperation({
+    description:'get outlet by Id',
+    summary:'show the details of outlet by outletId'
+  })
+  async getOutletById(
+    @Param('outletId',ParseIntPipe) outletId:number
+  ){
+    return this.outletService.getOutletById(outletId)
   }
 }
