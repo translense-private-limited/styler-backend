41d600065bfc06c45082934f894b2bf2a4f53332
Prasanthi-Traslense <prasanthi.kimidi@translense.com>
2024-12-05 14:58:15 +0530

    save changes

---
+++ src/modules/admin/Repositories/admin.repository.ts
@@ -0,0 +1,8 @@
+import { BaseRepository } from "@src/utils/repositories/base-repository";
+import { AdminEntity } from "../entities/admin.entity";
+
+export class AdminRepository extends BaseRepository<AdminEntity>{
+    constructor(
+        @Inject
+    )
+}
