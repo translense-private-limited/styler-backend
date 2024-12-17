0e0d02574f4d020a45b9057217f05ccfab60993a
Prasanthi-Traslense <prasanthi.kimidi@translense.com>
2024-12-17 16:30:40 +0530

    resloved lint issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -48,7 +48,7 @@ export default [
     rules: {
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-console':'error',
+      'no-console':'error',
       '@typescript-eslint/explicit-function-return-type': [
         'error',
         {
