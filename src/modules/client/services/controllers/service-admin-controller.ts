945185b681b181b9c6422ddd5e6bc33dc2e762b8
Prasanthi-Traslense <prasanthi.kimidi@translense.com>
2024-12-18 18:18:42 +0530

    developed admin- view list of services,serviceDetails,updateServiceDetails were developed

---
+++ src/modules/client/services/controllers/service-admin-controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Patch } from "@nestjs/common";
+import { ServiceSchema } from "../schema/service.schema";
+import { ServiceDto } from "../dtos/service.dto";
+import { ServiceService } from "../services/service.service";
+
+@Controller('admin')
+export class ServiceAdminController{
+    constructor(
+        private readonly serviceService:ServiceService
+    ){}
+
+    @Get('/outlet/:outletId/services')
+    async getAllServicesForOutlet(
+        @Param('outletId',ParseIntPipe) outletId:number
+    ):Promise<ServiceSchema[]>{
+        return this.serviceService.getAllServicesByOutletId(outletId)
+    }
+
+    @Get('services/:serviceId')
+    async getServiceDetails(
+        @Param('serviceId') serviceId:string
+    ):Promise<ServiceSchema>{
+        return this.serviceService.getServiceByIdOrThrow(serviceId)
+    }
+
+    @Patch('services/:serviceId')
+    async updateService(
+      @Param('serviceId') serviceId: string,
+      @Body() UpdateServiceDto: Partial<ServiceDto>,
+    ): Promise<ServiceSchema> {
+      return this.serviceService.updateServiceById(serviceId, UpdateServiceDto);
+    }
+
+}
