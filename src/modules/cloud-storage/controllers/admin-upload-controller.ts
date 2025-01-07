eb8a8abafdbd0876350e746cb1a1be334dce5a79
Prasanthi-Traslense <prasanthi.kimidi@translense.com>
2025-01-07 19:28:09 +0530

    resolved lint issues

--- src/modules/cloud-storage/controllers/admin-upload-controller.ts
+++ src/modules/cloud-storage/controllers/admin-upload-controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Put } from "@nestjs/common";
+import { Body, Controller, Put } from "@nestjs/common";
 import { KeyGeneratorDto } from "../dtos/key-generator.dto";
 import { UploadFilesService } from "../services/upload-files.service";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
