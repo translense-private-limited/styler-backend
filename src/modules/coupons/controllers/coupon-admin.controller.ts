db36e4b85d8f10a3d1a3b6dfa4b284ed369fd855
AshishK-TL <ashish.kumar@odriyo.com>
2025-02-27 18:58:13 +0530

    Global-coupon for outlet

--- src/modules/coupons/controllers/coupon-admin.controller.ts
+++ src/modules/coupons/controllers/coupon-admin.controller.ts
@@ -7,6 +7,7 @@ import {
   Body,
   HttpCode,
   Patch,
+  Put,
 } from '@nestjs/common';
 import { CouponService } from '../services/coupon.service';
 import { CreateCouponDto } from '../dtos/create-coupon.dto';
@@ -18,6 +19,7 @@ import { CouponEntity } from '../entities/coupon.entity';
 import { CouponOutletMappingEntity } from '../entities/coupon-outlet-mapping.entity';
 import { CouponClientService } from '../services/coupon-client.service';
 import { CreateCouponClientDto } from '../dtos/create-coupon-client.dto';
+import { AcceptRejectCouponCodeDto } from '../dtos/accept-reject-coupon-code.dto';
 
 @Controller('admin')
 @ApiTags('Coupons')
@@ -49,7 +51,7 @@ export class CouponAdminController {
     return this.couponService.getAll();
   }
 
-  @Get('coupons/outlet/:outletId')
+  @Get('outlet/:outletId/coupons')
   async getCouponsByOutletId(
     @Param('outletId') outletId: number,
   ): Promise<CouponInterface[]> {
@@ -68,17 +70,97 @@ export class CouponAdminController {
     return this.couponService.findOne(id);
   }
 
-  @Patch('coupon/:id')
-  async updateCoupon(
+  @Patch('coupon/:id/update')
+  async updateCouponForGlobal(
     @Param('id') id: number,
     @Body() updateCouponDto: Partial<CreateCouponDto>,
   ): Promise<CouponInterface> {
     return this.couponService.update(id, updateCouponDto);
   }
 
-  @Delete('coupon/:id')
+  @Delete('coupon/:id/delete')
   @HttpCode(204)
-  async deleteCoupon(@Param('id') id: number): Promise<void> {
+  async deleteCouponForGlobal(@Param('id') id: number): Promise<void> {
     return this.couponService.delete(id);
+  }
+
+  // API's for CouponsForOutlet
+
+  @Get('coupons/global')
+  async getGlobalCouponsForOutlet(): Promise<CouponEntity[]> {
+    return await this.couponClientService.getAllCouponCodePublishedByAdmin();
+  }
+
+  @Put('coupon/global/action')
+  async acceptRejectCouponCodeForGlobal(
+    @Body() acceptRejectCouponCodeDto: AcceptRejectCouponCodeDto,
+  ): Promise<boolean> {
+    await this.couponClientService.acceptRejectCouponCode(
+      acceptRejectCouponCodeDto,
+    );
+    return true;
+  }
+
+  @Get('outlet/:outletId/coupons/global/active')
+  async getAllActiveCouponCodeForOutlet(
+    @Param('outletId') outletId: number,
+  ): Promise<CouponEntity[]> {
+    return await this.couponClientService.getAllActiveGlobalCoupon(outletId);
+  }
+  @Get('outlet/:outletId/coupons/global/pending')
+  async getAllPendingGlobalCouponForOutlet(
+    @Param('outletId') outletId: number,
+  ): Promise<CouponEntity[]> {
+    return await this.couponClientService.getAllPendingGlobalCoupon(outletId);
+  }
+
+  @Patch('/outlet/:outletId/coupon/:couponId/update')
+  async updateCouponForOutlet(
+    @Param('couponId') couponId: number,
+    @Param('outletId') outletId: number,
+    @Body() updateCouponDto: Partial<CreateCouponDto>,
+  ): Promise<CouponInterface> {
+    return this.couponClientService.updateCoupon(
+      couponId,
+      outletId,
+      updateCouponDto,
+    );
+  }
+
+  @Delete('outlet/:outletId/coupon/:couponId/delete')
+  @HttpCode(204)
+  async deleteCouponForOutlet(
+    @Param('couponId') couponId: number,
+    @Param('outletId') outletId: number,
+  ): Promise<void> {
+    return this.couponClientService.delete(outletId, couponId);
+  }
+
+  @Get('coupon/outlet/:outletId/is-coupon-code-unique/:couponCode')
+  async isCouponCodeUniqueForOutlet(
+    @Param('couponCode') couponCode: string,
+    @Param('outletId') outletId: number,
+  ): Promise<CouponCheckResponseInterface> {
+    const response =
+      await this.couponClientService.doesCouponCodeExistForOutlet(
+        couponCode,
+        outletId,
+      );
+
+    if (!response) {
+      return {
+        success: true,
+        message: 'Coupon code is unique.',
+        isUnique: true,
+        coupon: couponCode,
+      };
+    }
+    return {
+      success: false,
+      message:
+        'The coupon code is already present with the provided code. Please choose a unique code.',
+      isUnique: false,
+      coupon: couponCode,
+    };
   }
 }
